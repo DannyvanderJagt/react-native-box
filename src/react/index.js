74187465cdbc576b66d96cd6325a329dace1af0c
Danny van der Jagt <danny@lightbase.nl>
2017-09-07 10:29:13 +0200

    changes v0.1.3

--- src/react/index.js
+++ src/react/index.js
@@ -1,3 +1,2 @@
-export { default as Portal } from './components/Portal'
 export { default as PresetPureComponent } from './PresetPureComponent'
 export { default as PresetComponent } from './PresetComponent'
